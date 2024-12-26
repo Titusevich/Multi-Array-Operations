5285e2c0f802ca30b6c0b15618e48e7fbd2e913e
Titusevich <nickitatitaevskiy@gmail.com>
2024-12-26 22:57:36 +0200

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,13 @@
+package app;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[][] matrix = {
+                {16, 5, 9, 21},
+                {7, 17, 7, 3},
+                {8, 14, 15, 5},
+                {3, 12, 4, 2}
+        };
+    }
+}
